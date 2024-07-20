73ac0987f29f9eb1422bd9a8877eb877b4fcd83c
sk-shahzad <shahzadd3v@gmail.com>
2024-07-20 11:57:29 -0700

    Admin and models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
-# Create your models here.
+
+class ChatRoom(models.Model):
+    host =  models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering =  ['-updated', '-created']
+
+    def __str__(self):
+        return self.name
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
+    chat_room = models.ForeignKey(ChatRoom, on_delete=models.CASCADE, related_name='messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.sender.username}: {self.content[:20]}'
+
+
+
+
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ChatRoom
+from .models import Message
 # Create your views here.
 
 def home(request):
-    return render(request ,'home.html')
+    room = ChatRoom.objects.all()
+    return render(request, 'home.html', {'room': room})
