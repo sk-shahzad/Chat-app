4c05ad7b3e6776629ba4871921958c8ee2e027ad
sk-shahzad <shahzadd3v@gmail.com>
2024-07-21 04:29:16 -0700

    Authentication

--- home/views.py
+++ home/views.py
@@ -2,12 +2,56 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from .models import Message
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+def loginPage(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        # print(f"Username:{username},Password:{password}")
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
 
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Username OR password does not exist')
+    context = {'page': page}
+    return render(request, 'login_register.html', context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('home')
+
+def registerPage(request):
+    page = 'register'
+    form = UserCreationForm()
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'An error has occured')
+
+    return render(request, 'login_register.html',{'form':form})
+
+@login_required(login_url='/login')
 def home(request):
-    # form = MessageForm()
-    # return render(request, 'home.html', {'form': form})
      chats = Message.objects.all()
      if request.method == 'POST':
         content = request.POST['content']
@@ -18,5 +62,4 @@ def home(request):
      context = {'chats': chats}
      return render(request, 'home.html', context)
 
-# def send(request):
 
